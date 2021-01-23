2832e0b22454522d8198eee1b706cd9f364830f5
Fatih NALBANT <fatihnalbant98@outlook.com>
2021-01-23 19:58:08 +0300

    Add project files.

---
+++ DictonaryExample/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictonaryExample
+{
+    class MyDictionary<Key, Value>
+     {
+        Key[] keys;
+        Value[] values;
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+        public void Add(Key key, Value value)
+        {
+            Key[] tempkeys = keys;
+            Value[] tempvalues = values;
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+            for (int i = 0; i < tempkeys.Length; i++)
+            {
+                keys[i] = tempkeys[i];
+                values[i] = tempvalues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " Yaş " + values[i]);
+            }
+        }
+
+    }
+}
---
+++ DictonaryExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//using System.Collections.Generic;
+
+namespace DictonaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(20, "Fatma");
+            myDictionary.Add(32, "Cabbar");
+            myDictionary.Print();
+        }
+    }
+}
